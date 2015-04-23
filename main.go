c826790e6872af73dc17655d4f6cd44aa5992275
hefju <hefju@qq.com>
2015-04-23 23:23:49 +0800

    在请求中加入log，但为什么输出到文件中不会换行呢？

--- main.go
+++ main.go
@@ -2,14 +2,36 @@ package main
 import (
     "net/http"
     "github.com/gin-gonic/gin"
+  //  "github.com/hefju/CBChareServer/setting"
 // "log"
     "time"
 //  "github.com/hefju/LXServer/tools"
 //  "strconv"
+    "log"
+    "os"
+   // "io"
 )
 func main(){
+
+    //配置初始化
+//    setting.LoadConfig()
+//    os.MkdirAll("D:/w8/logs",os.ModePerm)
+//    f,err:=os.Create("D:/w8/logs/cbserver.log")
+//    if err!=nil{
+//        log.Panic("create log file faild:",err)
+//    }
+//    w:=io.MultiWriter(f,os.Stdout)
+//    log.SetOutput(w)
+    logfile,err:=os.OpenFile("D:/w8/logs/cbserver.log",os.O_RDWR|os.O_CREATE,0)
+    if err!=nil{
+        log.Println("err")
+    }
+   // log.Println(logfile)
+   log.SetOutput(logfile)
+
     router := gin.Default()
     router.GET("/", func(c *gin.Context) {
+        log.Println("call index/n")
         c.String(http.StatusOK, "CBChareServer...")
     })
     //获取服务器时间
@@ -24,5 +46,6 @@ func main(){
 }
 
 func uploaddata(c *gin.Context) {
+    log.Println("call upload/n")
     c.String(http.StatusOK,"from method uploaddata")
 }
---
+++ setting/conf.go
@@ -0,0 +1,18 @@
+package setting
+//import (
+//    "os"
+//    "log"
+//    "io"
+//)
+func LoadConfig(){
+
+    //os.MkdirAll("./logs",os.ModePerm)
+//    os.MkdirAll("D:/w8/logs",os.ModePerm)
+//    f,err:=os.Create("D:/w8/logs/cbserver.log")
+//    if err!=nil{
+//        log.Panic("create log file faild:",err)
+//    }
+//    w:=io.MultiWriter(f,os.Stdout)
+//    log.SetOutput(w)
+
+}
